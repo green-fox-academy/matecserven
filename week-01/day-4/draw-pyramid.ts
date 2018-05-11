09b655b366614f3893b19570c034ee6883f4b404
Cserven Mate <cserk5@gmail.com>
2018-05-11 14:31:02 +0200

    Simplified pyramids

--- week-01/day-4/draw-pyramid.ts
+++ week-01/day-4/draw-pyramid.ts
@@ -5,14 +5,12 @@ export{}
 let lineCount: number = 4;
 let linePyr: string = '*';
 let lineSpace: string = ' ';
-let lineSpaceCount: number = 1;
-
 
 for (let k: number = 1; k <= lineCount; k++) {
-    for (lineSpace = ' '; lineSpaceCount < lineCount; lineSpaceCount++ ) {
+    for (let i: number = k; i < lineCount; i++ ) {
         lineSpace += ' ';       
     }  
     console.log(lineSpace + linePyr);
-    lineSpaceCount = k+1;
     linePyr += '**';
+    lineSpace = ' ';
 }
