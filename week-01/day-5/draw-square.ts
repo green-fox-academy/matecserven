ec6f541b32905e39b1365997e35e545f1b2b9da7
Cserven Mate <cserk5@gmail.com>
2018-05-11 09:41:39 +0200

    Square Done

---
+++ week-01/day-5/draw-square.ts
@@ -0,0 +1,23 @@
+'use strict';
+
+export{}
+
+let lineCount: number = 6;
+let squareLine: string = '';
+let squareMiddle: string = '#';
+
+
+for (let k: number = 0; k < lineCount; k++) {
+    squareLine += '#';
+}
+console.log(squareLine);
+
+for (let j: number = 2; j < lineCount; j++) {
+    squareMiddle += ' ';
+}
+
+for (let k: number = 2; k < lineCount; k++) {
+    console.log(squareMiddle + '#');
+}
+
+console.log(squareLine);
