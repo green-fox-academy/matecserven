d5ce4c3bb6a1aea22977b5d638ba9195cf1e62c3
Cserven Mate <cserk5@gmail.com>
2018-05-11 10:30:23 +0200

    Adding chess table

---
+++ week-01/day-5/draw-chess-table.ts
@@ -0,0 +1,11 @@
+'use strict';
+
+export {}
+
+for (let k: number = 1; k <= 8; k++) {
+    if (k % 2 == 0) {
+        console.log('# # # #');
+    } else {
+        console.log(' # # # #');
+    }
+}
