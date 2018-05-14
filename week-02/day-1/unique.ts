2c1953c340613e1e8ce5746a8e8268224e76cac2
Cserven Mate <cserk5@gmail.com>
2018-05-14 21:59:33 +0200

    Done unique.ts

---
+++ week-02/day-1/unique.ts
@@ -0,0 +1,12 @@
+'use strict';
+export {}
+
+//  Create a function that takes a list of numbers as a parameter
+//  Returns a list of numbers where every number in the list occurs only once
+function unique(arr): number[] {
+  return arr.sort().filter(function(value, index, self) {
+    return (index === self.indexOf(value));
+  });
+}
+
+console.log(unique([1, 11, 34, 11, 52, 61, 1, 34]))
