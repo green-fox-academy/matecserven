5c6abd8013af727f56336ea03f0c2a31347fbfc4
Cserven Mate <cserk5@gmail.com>
2018-05-14 13:57:35 +0200

    done change-elements.ts

---
+++ week-02/day-1/change-elements.ts
@@ -0,0 +1,18 @@
+'use strict';
+export{}
+
+
+// -  Create an array named `numList` with the following content: `[1, 2, 3, 8, 5, 6]`
+// -  Change the 8 to 4 with the `.map` method 
+// -  Print the fourth element as a test
+
+let numList = [1, 2, 3, 8, 5, 6];
+
+numList = numList.map(x => {
+  if (x == 8) {
+    x = 4;
+  } 
+  return x;
+});
+
+console.log(numList[3]);
