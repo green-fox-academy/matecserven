e5694fba5908e148fcf95c8160d837614c7a8985
Cserven Mate <cserk5@gmail.com>
2018-05-14 13:15:20 +0200

    Done swap-elements.ts

---
+++ week-02/day-1/swap-elements.ts
@@ -0,0 +1,14 @@
+'use strict';
+export{}
+
+// -  Create a variable named `abc` with the following content: `["Arthur", "Boe", "Chloe"]`
+// -  Swap the first and the third element of `abc`
+
+const abc = ["Arthur", "Boe", "Chloe"];
+
+const cba = abc.shift();
+
+abc.splice(0,0,abc.pop());
+abc.push(cba);
+
+console.log(abc);
