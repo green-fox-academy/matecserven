e490e8e82232664805f12cceb87b07368b5f6b5e
Cserven Mate <cserk5@gmail.com>
2018-05-14 13:15:03 +0200

    Fixed sum.ts to solve execise properly

--- week-02/day-1/sum.ts
+++ week-02/day-1/sum.ts
@@ -4,6 +4,7 @@ export{}
 // -  Write a function called `sum` that sum all the numbers until the given parameter
 // -  The function should return the result
 
+/* //sum of more arguments
 function sum(...numbers: number[]) {
   let sumofsum: number = 0;
   for(let i: number = 0; i < numbers.length; i++) {
@@ -14,3 +15,14 @@ function sum(...numbers: number[]) {
 
 sum(1,2,3,4,5);
 sum(12,13);
+*/
+
+function sum(num: number): number {
+  let summa: number = 0;
+  for(let i: number = 1; i <= num; i++){
+    summa += i;
+  }
+  return summa;
+}
+
+console.log(sum(5));
