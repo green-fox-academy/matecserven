0ac1cb7b66f229ee1e452368804bb5170a297634
Cserven Mate <cserk5@gmail.com>
2018-05-15 10:41:03 +0200

    done reverse.ts

---
+++ week-02/day-2/reverse.ts
@@ -0,0 +1,16 @@
+'use strict';
+export{}
+// Create a function that can reverse a string, which is passed as the parameter
+// Use it on this reversed string to check it!
+function reverse(input: string): string {
+  let correct: string = '';
+  for(let i: number = 0; i <= input.length; i++) {
+    correct += input.charAt(input.length - i);
+  };
+  return correct;
+}
+
+let reversed: string = '.eslaf eb t\'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI';
+console.log(reverse(reversed));
+
+export = reverse;
