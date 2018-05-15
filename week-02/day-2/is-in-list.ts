967ef22a668276e4ab8e670c48e8e6ed49e2e03a
Cserven Mate <cserk5@gmail.com>
2018-05-15 15:03:15 +0200

    done is-in-list.ts

---
+++ week-02/day-2/is-in-list.ts
@@ -0,0 +1,17 @@
+'use strict';
+// Check if array contains all of the following elements: 4,8,12,16
+// Create a function that accepts 'listOfNumbers' as an input
+// it should return "true" if it contains all, otherwise "false"
+let listOfNumbers: number[] = [2, 4, 6, 8, 10, 12, 14, 16];
+
+function checkNums(input: number[]): boolean {
+  let op: boolean = false;
+  if(-1 <
+  input.indexOf(4) && input.indexOf(8) && 
+  input.indexOf(12) && input.indexOf(16)) {op = true;}
+  return op;
+}
+
+console.log(checkNums(listOfNumbers));
+
+export = checkNums;
