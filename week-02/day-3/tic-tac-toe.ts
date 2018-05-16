c251db9996deeaa3e494d8e63c48ca2e5cd24c71
Cserven Mate <cserk5@gmail.com>
2018-05-16 16:26:47 +0200

    added error handling to tictac

--- week-02/day-3/tic-tac-toe.ts
+++ week-02/day-3/tic-tac-toe.ts
@@ -11,37 +11,41 @@ let fs = require('fs');
 let charEncoding = 'utf-8'
 
 function ticTacResult(filePath: string): string {
-  let array: string[];
-  let matrix: string[][] = [];
-  array = fs.readFileSync(filePath, charEncoding).split('\n');
-  for (let i: number = 0; i < 3; i++) {
-    matrix[i] = array[i].split('');
-  }
-  if (matrix[0][0] === matrix[0][1] && matrix[0][2] === matrix[0][0]) {
-    return matrix[0][0];
-  } else if
-  (matrix[0][0] === matrix[1][1] && matrix[2][2] === matrix[0][0]) {
-    return matrix[0][0];
-  } else if
-  (matrix[0][0] === matrix[1][0] && matrix[2][0] === matrix[0][0]) {
-    return matrix[0][0];
-  } else if
-  (matrix[1][0] === matrix[1][1] && matrix[1][2] === matrix[1][0]) {
-    return matrix[1][0];
-  } else if
-  (matrix[2][0] === matrix[2][1] && matrix[2][2] === matrix[2][0]) {
-    return matrix[2][0];
-  } else if
-  (matrix[0][1] === matrix[1][1] && matrix[2][1] === matrix[0][1]) {
-    return matrix[0][1];
-  } else if
-  (matrix[0][2] === matrix[1][2] && matrix[2][2] === matrix[0][2]) {
-    return matrix[0][2];
-  } else if
-  (matrix[0][2] === matrix[1][1] && matrix[2][0] === matrix[0][2]) {
-    return matrix[0][2];
-  } else {
-    return 'Draw';
+  try {
+    let array: string[];
+    let matrix: string[][] = [];
+    array = fs.readFileSync(filePath, charEncoding).split('\n');
+    for (let i: number = 0; i < 3; i++) {
+      matrix[i] = array[i].split('');
+    }
+    if (matrix[0][0] === matrix[0][1] && matrix[0][2] === matrix[0][0]) {
+      return matrix[0][0];
+    } else if
+    (matrix[0][0] === matrix[1][1] && matrix[2][2] === matrix[0][0]) {
+      return matrix[0][0];
+    } else if
+    (matrix[0][0] === matrix[1][0] && matrix[2][0] === matrix[0][0]) {
+      return matrix[0][0];
+    } else if
+    (matrix[1][0] === matrix[1][1] && matrix[1][2] === matrix[1][0]) {
+      return matrix[1][0];
+    } else if
+    (matrix[2][0] === matrix[2][1] && matrix[2][2] === matrix[2][0]) {
+      return matrix[2][0];
+    } else if
+    (matrix[0][1] === matrix[1][1] && matrix[2][1] === matrix[0][1]) {
+      return matrix[0][1];
+    } else if
+    (matrix[0][2] === matrix[1][2] && matrix[2][2] === matrix[0][2]) {
+      return matrix[0][2];
+    } else if
+    (matrix[0][2] === matrix[1][1] && matrix[2][0] === matrix[0][2]) {
+      return matrix[0][2];
+    } else {
+      return 'Draw';
+    }
+  } catch (error) {
+    return ('404');
   }
 }
 
