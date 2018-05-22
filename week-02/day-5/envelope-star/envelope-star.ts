7a0a8ac85705c05576fb6fd0c629197ab719f77f
Cserven Mate <cserk5@gmail.com>
2018-05-22 13:45:24 +0200

    Fixed envelope stars function

--- week-02/day-5/envelope-star/envelope-star.ts
+++ week-02/day-5/envelope-star/envelope-star.ts
@@ -3,16 +3,18 @@
 const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
 const ctx = canvas.getContext('2d');
 
-function star(x: number) {
-  for (let i: number = 0; i < canvas.width / 2; i += x) {
-    ctx.strokeStyle = 'green';
-    ctx.beginPath();
-    ctx.moveTo(canvas.width / 2, i);
-    ctx.lineTo(canvas.width / 2 - i, canvas.height / 2);
-    ctx.lineTo(canvas.width / 2, canvas.height - i);
-    ctx.lineTo(canvas.width / 2 + i, canvas.height / 2);
-    ctx.closePath();
-    ctx.stroke();
-  }
+function star(x: number): void {
+  ctx.strokeStyle = 'green';
+  ctx.beginPath();
+  ctx.moveTo(canvas.width / 2, x);
+  ctx.lineTo(canvas.width / 2 - x, canvas.height / 2);
+  ctx.lineTo(canvas.width / 2, canvas.height - x);
+  ctx.lineTo(canvas.width / 2 + x, canvas.height / 2);
+  ctx.closePath();
+  ctx.stroke();
 }
-star(5);
+
+let y: number = 10; // Set size between lines
+for (let i: number = 0; i < canvas.width / 2; i += y) {
+  star(i);
+}
