33d82abca067ba2fa8eb8c215f73ea420d089f21
Cserven Mate <cserk5@gmail.com>
2018-05-24 10:27:33 +0200

    done sum-digit.ts

---
+++ week-03/day-4/sum-digit.ts
@@ -0,0 +1,11 @@
+'use strict';
+export { }
+
+function sumDigit(n: number): number {
+  if (n < 1) {
+    return 0;
+  }
+  return (n % 10) + sumDigit(Math.floor(n / 10));
+}
+
+console.log(sumDigit(126));
