c647540b2258b20dc503cfa71a746251666cd358
Cserven Mate <cserk5@gmail.com>
2018-06-25 15:22:28 +0200

    Done do until

--- week-07/day-1/workshop/routes.js
+++ week-07/day-1/workshop/routes.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const app = express();
 
-
+app.use(express.json());
 app.use('/assets', express.static('assets'));
 
 app.get('/', (req, res) => {
@@ -27,16 +27,56 @@ app.get('/greeter', (req, res) => {
   if (req.query.name && req.query.title) {
     res.json({
       welcome_message: `Oh, hi there ${req.query.name}, my dear ${req.query.title}!`,
-    })
+    });
   } else if (req.query.name === undefined && req.query.title){
     res.json({
       error: "Please provide a name!",
-    })
+    });
   } else if (req.query.name && req.query.title === undefined) {
     res.json({
       error: "Please provide a title!",
-    })
+    });
+  } else if (req.query.name === undefined && req.query.title === undefined) {
+    res.json({
+      error: "Please provide a name!",
+    });
   }
+});
+
+app.get('/appenda/:appendable', (req, res) => {
+  if (req.params.appendable) {
+    res.json({
+      appended: req.params.appendable + 'a',
+    });
+  }
+});
+
+app.get('/appenda/', (req, res) => {
+  res.status(404).send('No input provided');
+})
+
+app.post('/dountil/:what', (req, res) => {
+  let message = { };
+  let until = req.body.until;
+  let what = req.params.what;
+  let fin = 1;
+  if (until === undefined) {
+    message = { error: "Please provide a number!"};
+  } else {
+    if (what === "sum") {
+      for (let i = 1; i < until; i++) {
+        fin += i + 1;
+      }
+      message = { result: `${fin}`};
+    }
+    if (what === "factor") {
+      for (let i = 1; i < until; i++) {
+        fin *= i + 1;
+      }
+      message = { result: `${fin}` };
+    }
+  }
+  res.json(message);
 })
 
 module.exports = app;
