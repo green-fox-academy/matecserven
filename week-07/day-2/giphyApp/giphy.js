152dc4a1c7fdef4a62a58dfc08852d3642957223
Cserven Mate <cserk5@gmail.com>
2018-06-26 15:22:40 +0200

    refactored giphy JS file

--- week-07/day-2/giphyApp/giphy.js
+++ week-07/day-2/giphyApp/giphy.js
@@ -3,7 +3,6 @@
 const gif = document.querySelector('#gif');
 const img = document.createElement('iframe');
 const thumbnail = document.querySelector('footer');
-let data;
 
 let xhr = new XMLHttpRequest(),
   method = "GET",
@@ -13,14 +12,14 @@ let xhr = new XMLHttpRequest(),
 xhr.open(method, url, true);
 xhr.onreadystatechange = function () {
   if (xhr.readyState === 4 && xhr.status === 200) {
-    data = JSON.parse(xhr.responseText);
-    console.log(data);
-    img.setAttribute('src', data.data[0].images.original.url);
+    const response = JSON.parse(xhr.responseText);
+    console.log(response);
+    img.setAttribute('src', response.data[0].images.original.url);
     gif.appendChild(img);
 
-    data.data.forEach((data, index) => {
+    response.data.forEach(data => {
       const newPic = document.createElement('img');
-      newPic.setAttribute('src', data.images.original.url);
+      newPic.setAttribute('src', data.images.fixed_width_still.url);
       newPic.classList.add('thumbs');
       newPic.onclick = () => {
         document.querySelector('iframe').setAttribute('src', data.images.original.url);
