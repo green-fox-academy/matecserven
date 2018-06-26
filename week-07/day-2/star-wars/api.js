69d91aec6df38992e194df7329188394e70957b3
Cserven Mate <cserk5@gmail.com>
2018-06-26 17:44:18 +0200

    Finding character names

--- week-07/day-2/star-wars/api.js
+++ week-07/day-2/star-wars/api.js
@@ -4,14 +4,22 @@ let input = document.querySelector('#inputField').value;
 const list = document.querySelector('ul');
 
 const http = new XMLHttpRequest();
+http.open('GET', `https://swapi.co/api/people/`, true);
 http.onload = () => {
   const response = JSON.parse(http.responseText);
   console.log(response);
+  
+  response.results.some(data => {
+    if (data.name === input) {
+      return console.log('good');
+    } else {
+      console.log('lel');
+    }
+  });
 }
 
 const button = document.querySelector('#search');
 button.addEventListener('click', () => {
   input = document.querySelector('#inputField').value;
-  http.open('GET', `https://swapi.co/api/${input}`, true);
   http.send();
 });
