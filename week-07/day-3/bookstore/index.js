a892b6c793a4301e4c06a81ccdcb8c362e41e979
Cserven Mate <cserk5@gmail.com>
2018-06-27 11:06:17 +0200

    Adding dotenv

--- week-07/day-3/bookstore/index.js
+++ week-07/day-3/bookstore/index.js
@@ -1,15 +1,16 @@
 'use strict';
 
+require('dotenv').config();
 const express = require('express');
 const mysql = require('mysql');
 const app = express();
 const PORT = 3000;
 
 const conn = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'root',
-  database: 'bookstore',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
 });
 
 app.get('/test', (req, res) => {
