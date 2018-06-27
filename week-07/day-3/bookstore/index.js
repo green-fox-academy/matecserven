f6c31d5979e1e32f70786cf8c6ec6a991c78abfe
Cserven Mate <cserk5@gmail.com>
2018-06-27 11:03:53 +0200

    Revert "Added dotenv protection"
    This reverts commit a6b3071b01361f5e6f66d2344c36f39039ccd51f.

--- week-07/day-3/bookstore/index.js
+++ week-07/day-3/bookstore/index.js
@@ -1,16 +1,15 @@
 'use strict';
 
-require('dotenv').config();
 const express = require('express');
 const mysql = require('mysql');
 const app = express();
 const PORT = 3000;
 
 const conn = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
+  host: 'localhost',
+  user: 'root',
+  password: 'root',
+  database: 'bookstore',
 });
 
 app.get('/test', (req, res) => {
