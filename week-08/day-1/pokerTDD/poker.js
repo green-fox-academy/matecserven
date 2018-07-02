0a573b80b775b821dcf0ab3fcf94f459dc4f3618
Cserven Mate <cserk5@gmail.com>
2018-07-02 17:27:47 +0200

    Added pair comparing

--- week-08/day-1/pokerTDD/poker.js
+++ week-08/day-1/pokerTDD/poker.js
@@ -2,44 +2,75 @@
 
 function poker(p1, p2) {
   let winner = '';
-  let highP1 = 0;
-  let highP2 = 0;
-  let vIndex = 0;
-  let color = '';
   let value = [];
+  let sortedP1 = [];
+  let sortedP2 = [];
+  let pairP1 = [];
+  let pairP2 = [];
   const stock = ['2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A'];
 
   p1.forEach(num => {
     value = num.split('');
     stock.forEach((el, index) => {
       if (value[1] === el) {
-        vIndex = index;
+        sortedP1.push(index);
       }
-    })
-    if (vIndex > highP1) {
-      highP1 = vIndex;
-    }
+    });
   });
 
   p2.forEach(num => {
     value = num.split('');
     stock.forEach((el, index) => {
       if (value[1] === el) {
-        vIndex = index;
+        sortedP2.push(index);
       }
-    })
-    if (vIndex > highP2) {
-      highP2 = vIndex;
+    });
+  });
+
+  sortedP1.sort((a, b) => {
+    return a - b;
+  });
+  sortedP2.sort((a, b) => {
+    return a - b;
+  });
+
+  sortedP1.forEach((el, index) => {
+    if (el === sortedP1[index + 1]) {
+      pairP1.push(el);
     }
   });
 
-  if (highP1 > highP2) {
-    winner = 'p1';
-  } else if (highP1 < highP2) {
+  sortedP2.forEach((el, index) => {
+    if (el === sortedP2[index + 1]) {
+      pairP2.push(el);
+    }
+  });
+
+
+  //pair
+  if ((pairP1.length !== 0) && (pairP2.length !== 0)) {
+    if (pairP1[0] > pairP2[0]) {
+      winner = 'p1';
+    } else if (pairP1[0] < pairP2[0]) {
+      winner = 'p2';
+    } else {
+      winner = 'drawpair';
+    }
+  } else if ((pairP1.length === 0) && (pairP2.length !== 0)) {
     winner = 'p2';
+  } else if ((pairP1.length !== 0) && (pairP2.length === 0)) {
+    winner = 'p1';
   } else {
-    winner = 'draw';
+    //HC
+    if (sortedP1[4] > sortedP2[4]) {
+      winner = 'p1';
+    } else if (sortedP1[4] < sortedP2[4]) {
+      winner = 'p2';
+    } else {
+      winner = 'drawHC';
+    }
   }
+
 
   return winner;
 }
--- week-08/day-1/pokerTDD/poker.test.js
+++ week-08/day-1/pokerTDD/poker.test.js
@@ -4,9 +4,9 @@ const test = require('tape');
 const poker = require('./poker');
 
 test('test poker', t => {
-  const p1 = ['CT', 'CQ', 'D3', 'DA', 'C6'];
-  const p2 = ['S3', 'HT', 'SK', 'D6', 'CK'];
+  const p1 = ['CT', 'CQ', 'HQ', 'DA', 'C6'];
+  const p2 = ['S3', 'HQ', 'SQ', 'D2', 'C5'];
 
-  t.equal(poker(p1, p2), 'p1');
+  t.equal(poker(p1, p2), 'drawpair');
   t.end();
 });
