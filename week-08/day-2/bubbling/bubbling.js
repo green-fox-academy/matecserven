f405885a5b426fd9471a4d5087176717ac5ed283
Cserven Mate <cserk5@gmail.com>
2018-07-03 14:05:24 +0200

    Added up,down button

--- week-08/day-2/bubbling/bubbling.js
+++ week-08/day-2/bubbling/bubbling.js
@@ -4,6 +4,8 @@ const nav = document.querySelector('nav');
 const img = document.querySelector('.img-inspector');
 const style = document.querySelector('style');
 let size = 200;
+let marginTop = 0;
+let marginLeft = 0;
 
 nav.addEventListener('click', (event) => {
   if (event.target.getAttribute('data-direction') === 'in') {
@@ -15,4 +17,15 @@ nav.addEventListener('click', (event) => {
     size += 20;
     img.style.backgroundSize = `${size}%`;
   }
+
+  if (event.target.getAttribute('data-direction') === 'up') {
+    marginTop -= 10;
+    img.style.marginTop = `${marginTop}px`;
+  }
+
+  if (event.target.getAttribute('data-direction') === 'down') {
+    marginTop += 10;
+    img.style.marginTop = `${marginTop}px`;
+  }
+
 });
