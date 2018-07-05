afb530347a551ac6cae1f0087d688579e5b2a138
Cserven Mate <cserk5@gmail.com>
2018-07-05 15:50:58 +0200

    Added arrows

--- week-07/day-4/reddit_project/src/static/js/index.js
+++ week-07/day-4/reddit_project/src/static/js/index.js
@@ -5,14 +5,24 @@ const host = 'http://localhost:3000';
 const postList = document.querySelector('.postList');
 
 function createPost(element) {
+  const upvote = document.createElement('img');
+  upvote.setAttribute('src', 'static/imgs/upvote.png');
+  upvote.classList.add('upvote');
+  const downvote = document.createElement('img');
+  downvote.setAttribute('src', 'static/imgs/downvote.png');
+  downvote.classList.add('downvote');
+  
   const post = document.createElement('div');
   post.classList.add('post');
+  post.setAttribute('id', element.id);
   postList.appendChild(post);
 
   const score = document.createElement('div');
   score.classList.add('score');
-  score.textContent = `${element.score}`;
   post.appendChild(score);
+  score.textContent = element.score;
+  score.appendChild(upvote);
+  score.appendChild(downvote);
   const details = document.createElement('div');
   details.classList.add('details');
   post.appendChild(details);
@@ -21,31 +31,30 @@ function createPost(element) {
   cellTitle.classList.add('cell');
   cellTitle.classList.add('title');
   details.appendChild(cellTitle);
-  cellTitle.textContent = `${element.title}`;
+  cellTitle.textContent = element.title;
   const cellUrl = document.createElement('div');
   cellUrl.classList.add('cell');
   cellUrl.classList.add('url');
   details.appendChild(cellUrl); 
-  cellUrl.textContent = `${element.url}`;
+  cellUrl.textContent = element.url;
   const cellOwner = document.createElement('div');
   cellOwner.classList.add('cell');
   cellOwner.classList.add('owner');
   details.appendChild(cellOwner);
-  cellOwner.textContent = `${element.owner}`;
+  cellOwner.textContent = element.owner;
   const cellDate = document.createElement('div');
   cellDate.classList.add('cell');
   cellDate.classList.add('date');
   details.appendChild(cellDate);
-  cellDate.textContent = `${element.timestamp}`;
+  cellDate.textContent = element.timestamp;
 }
 
 http.open('GET', `${host}/api/posts`, true);
-
+http.setRequestHeader('username', 'user1');
 http.onload = () => {
   const response = JSON.parse(http.responseText);
   response.posts.forEach(element => {
     createPost(element);
   });
 }
-
 http.send();
