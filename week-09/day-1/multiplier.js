c6e7a8a21c5d97294b256170fa1d4c97de7d1275
Cserven Mate <cserk5@gmail.com>
2018-07-09 11:57:19 +0200

    Done multiplier exec

---
+++ week-09/day-1/multiplier.js
@@ -0,0 +1,19 @@
+'use strict';
+
+function multiplier(number) {
+  return function(value) {
+    return number * value;
+  }
+}
+
+// Create the multiplier function that you can use like this:
+
+const duplicator = multiplier(2);
+
+console.log(duplicator(5)); // should log 10
+console.log(duplicator(10)); // should log 20
+
+const threeTimes = multiplier(3);
+
+console.log(threeTimes(5)); // should log 15
+console.log(threeTimes(100)); // should log 300
