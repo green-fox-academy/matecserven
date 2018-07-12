e5f394fe6b3b38b4150eafb4cb3541e973d3c83e
Cserven Mate <cserk5@gmail.com>
2018-07-12 15:30:45 +0200

    Done question cycling

--- week-09/day-4/quizApp/src/static/js/index.js
+++ week-09/day-4/quizApp/src/static/js/index.js
@@ -4,9 +4,17 @@ const http = new XMLHttpRequest();
 const host = 'http://localhost:3000';
 const question = document.querySelector('.question');
 const answersList = document.querySelector('.answers');
+const scoreSpan = document.querySelector('span');
+let score = 0;
 
 window.onload = () => {
   getQuestion();
+  addClick();
+  setInterval(() => {
+    answersList.innerHTML = '';
+    getQuestion();
+    addClick();
+  }, 10000);
 }
 
 const getQuestion = () => {
@@ -28,4 +36,18 @@ const listAnswers = (element) => {
   answer.dataset.correct = element.is_correct;
   answer.textContent = element.answer;
   answersList.appendChild(answer);
+}
+
+const addClick = () => {
+   answersList.addEventListener('click', (event) => {
+    if (event.target.className === 'answer') {
+      if (event.target.dataset.correct === '1') {
+        event.target.style.background = "green";
+        score++;
+        scoreSpan.textContent = score;
+      } else {
+        event.target.style.background = "red";
+      }
+    }
+  });
 }
