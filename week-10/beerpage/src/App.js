39bd22de89d62319ee182e05c474d0d9dba38e39
Cserven Mate <cserk5@gmail.com>
2018-08-01 14:40:24 +0200

    Added lot of progress(fetch API, onClick)

--- week-10/beerpage/src/App.js
+++ week-10/beerpage/src/App.js
@@ -4,15 +4,44 @@ import BeerTile from './components/BeerTile';
 import Paginator from './components/Paginator';
 
 class App extends Component {
+  state = {
+    name: undefined,
+    description: undefined,
+    imgUrl: undefined,
+  }
+
+  getBeers = () => {
+    fetch('https://api.punkapi.com/v2/beers/random')
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        this.setState({
+          name: data[0].name,
+          description: data[0].description,
+          imgUrl: data[0].image_url,
+        });
+        console.log(this.state);
+      })
+  }
+
+  componentDidMount() {
+    this.getBeers();
+  }
+
   render() {
     return (
       <div>
         <Header />
-        <BeerTile />
+        <BeerTile
+          name={this.state.name}
+          description={this.state.description}
+          imgUrl={this.state.imgUrl}
+        />
         <Paginator />
       </div>
     );
   }
+
 }
 
 export default App;
--- week-10/beerpage/src/components/BeerTile.js
+++ week-10/beerpage/src/components/BeerTile.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const BeerTile = () => (
-  <div>
-    <img alt="beer" />
-    <p>Beer Name</p>
-    <p>Description</p>
-  </div>
-);
+class BeerTile extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isToggleOn: false
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState(prevState => ({
+      isToggleOn: !prevState.isToggleOn
+    }));
+  }
+
+  render() {
+    return (
+      <div className="card">
+        <div className="content" onClick={this.handleClick}>
+          {this.state.isToggleOn ?
+            <p>{this.props.description}</p>
+            : <img src={this.props.imgUrl} alt="beer" />}
+        </div>
+        <p>{this.props.name}</p>
+      </div>
+    )
+  }
+}
+
+
 
 export default BeerTile;
